6cb22095dd3292c89c764c672aaf26a49ca66b93
Bojan Vlasonjic <bojanvlasonjic@yahoo.com>
2019-05-21 19:28:10 +0200

    Initial commit

---
+++ MonumentMap/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Maps.MapControl.WPF; /* Direktiva za mapu i njene elemente */
+
+namespace MonumentMap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            worldMap.MouseDoubleClick += new MouseButtonEventHandler(worldMap_MouseDoubleClick);
+            worldMap.ViewChangeOnFrame += new EventHandler<MapEventArgs>(worldMap_ViewChangeOnFrame);
+        }
+
+        //na dupli klik se ubaci pin na mapu - za sad
+        private void worldMap_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            e.Handled = true;
+
+            Point mousePosition = e.GetPosition(this);
+            Location pinLocation = worldMap.ViewportPointToLocation(mousePosition);
+
+            ControlTemplate template = (ControlTemplate)this.FindResource("MonumentPinTemplate"); //template za promenu izgleda pin-a
+
+            Pushpin pin = new Pushpin();
+            pin.Template = template;
+            pin.Location = pinLocation;
+            pin.Content = "pin";
+
+            worldMap.Children.Add(pin);
+        }
+
+        //postavljanje granice za zoom
+        private void worldMap_ViewChangeOnFrame(object sender, MapEventArgs e)
+        {
+            double z = worldMap.ZoomLevel;
+
+            if (z > 8)
+            {
+                worldMap.ZoomLevel = 8;
+            }
+
+            if (z < 2)
+            {
+                worldMap.ZoomLevel = 2;
+            }
+        }
+    }
+}
